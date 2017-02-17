6d1e36c135ed8d8ecce1a5dff1213a4a5f03ad75
Lukas Juhas <hello@lukasjuhas.com>
2017-02-17 17:58:13 +0000

    add release id

--- app/Transformers/RecordTransformer.php
+++ app/Transformers/RecordTransformer.php
@@ -7,10 +7,10 @@ class RecordTransformer extends Transformer
     public function transform($record)
     {
         return [
+            'release_id' => $record->release_id,
             'title' => $record->basic_information->title,
             'artist' => $record->basic_information->artists[0]->name,
             'year' => $record->basic_information->year
-            // 'active' => (boolean) $record['some_bool'],
         ];
     }
 }
